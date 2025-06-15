c4360f066d0217ee6b79c8caf48315c552b3cc88
mrbravogil <mrbravogil@gmail.com>
2025-06-15 12:54:23 +0200

    c perfil

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -6,12 +6,12 @@ import { Component } from '@angular/core';
   template: `
   <div class="flex flex-col gap-20 lg:pr[20%] ">
   <div class="flex flex-col gap-12">
-        <img src="assets/img/mariarosabravo-img.jpeg" alt="Foto de perfil" class="w-55 h-55 rounded-full object-cover shadow-lg border-4 border-white">
-      <h1 class="text-4xl">María Rosa Bravo Gil</h1>
-      <p class="font-lexend text-2xl"><b class="text-purple-400">Desarrolladora de software junior</b>, enfocada  en el Fullstack. Actualmente apostando por <b class="text-purple-200">Angular & React</b>,   mejorando cada día. De Fuengirola (Málaga), España 🇪🇸</p>
+        <img src="assets/img/mariarosabravo-img.jpeg" alt="Foto de perfil" class="w-40 h-40 rounded-full object-cover shadow-lg border-4 border-white">
+      <h1 class="text-6xl">María Rosa Bravo Gil</h1>
+      <p class="font-lexend text-4xl"><b class="text-purple-400">Desarrolladora de software junior</b>, enfocada  en el Fullstack. Actualmente apostando por <b class="text-purple-200">Angular & React</b>,   mejorando cada día. De Fuengirola (Málaga), España 🇪🇸</p>
       </div>
       <div class="flex flex-col gap-12">
-        <div class="flex flex-wrap gap-12 text-xl">
+        <div class="flex flex-wrap gap-12 text-lg">
           <a href="https://www.linkedin.com/in/mrbravogil" target="_blank" class="flex items-center justify-center gap-4 bg-gray-900 rounded-4xl p-4 border border-purple-400 hover:bg-black">
             <img src="assets/img/linkedin-logo.png" class="w-15" alt="">LinkedIn
           </a>
