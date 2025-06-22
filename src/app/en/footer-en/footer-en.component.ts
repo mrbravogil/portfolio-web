6513e1c1d09ff6d0f11b5baa7fa44d8a55a19756
mrbravogil <mrbravogil@gmail.com>
2025-06-22 19:40:41 +0200

    footer

--- src/app/en/footer-en/footer-en.component.ts
+++ src/app/en/footer-en/footer-en.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
   <div class="p-10 mt-15 flex items-center justify-center border-t-4 border-t-purple-400">
-    <p class="text-white text-center text-md lg:text-lg lg:p-10">Developed with Angular & TailwindCSS</p>
+    <p class="text-white text-center text-md lg:text-lg lg:p-10">Built using Angular & TailwindCSS</p>
 
 </div>
   `,
