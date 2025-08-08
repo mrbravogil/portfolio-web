24f7c5c67592eb8743a833ee46eb05f338bc5ede
mrbravogil <mrbravogil@gmail.com>
2025-08-08 16:01:14 +0200

    instalacion i18n, etiquetado y traduccion.

--- src/app/home/components/side-menu/proyectos/proyectos.component.ts
+++ src/app/home/components/side-menu/proyectos/proyectos.component.ts
@@ -14,16 +14,16 @@ export class ProyectosComponent {
   proyectosList: Proyecto[] = [
     {
       id: 0,
-      name: 'Rotten Tomatoes',
-      descripcion: 'Clon funcional del sitio Rotten Tomatoes con React Router y Tailwind con conexión a datos desde el Frontend. Versión con Backend en repositorio Github usando Node.js, Express, Prisma y MySQL.',
+      name: $localize`:@@proj.rotten-tomatoes.name:Rotten Tomatoes`,
+      descripcion: $localize`:@@proj.rotten-tomatoes.desc:Clon funcional del sitio Rotten Tomatoes con React Router y Tailwind con conexión a datos desde el Frontend. Versión con Backend en repositorio Github usando Node.js, Express, Prisma y MySQL.`,
       photoUrl: 'assets/img/rotten-tomatoes-img.png',
       linkgithub: 'https://github.com/mrbravogil/Rotten-Tomatoes-Clone',
       linkpreview: 'https://rotten-tomatoes-clone-maria-rosa-bravo-s-projects.vercel.app/',
     },
     {
       id: 1,
-      name: 'Tarjeta Pregunta - Trivial Personalizable',
-      descripcion: 'Componente dinámico de tarjeta de preguntas construido con React, Tailwind CSS, Node.js y Express, utilizando Prisma como ORM y MySQL como base de datos.',
+      name:  $localize`:@@proj.trivial.name:Tarjeta Pregunta - Trivial Personalizable`,
+      descripcion: $localize`:@@proj.trivial.desc:Componente dinámico de tarjeta de preguntas construido con React, Tailwind CSS, Node.js y Express, utilizando Prisma como ORM y MySQL como base de datos.`,
       photoUrl: 'assets/img/custom-trivial-img.png',
       linkgithub: 'https://github.com/mrbravogil/Trivial-Customizable',
       linkpreview: 'https://youtu.be/QwUGD5fFMAw',
--- src/app/home/shared/header/header.component.ts
+++ src/app/home/shared/header/header.component.ts
@@ -5,14 +5,7 @@ import { RouterModule } from '@angular/router';
   selector: 'app-header',
   standalone: true,
   imports: [RouterModule],
-  template: `
-  <div class="flex p-10 text-xl absolute right-15 top-5 gap-3">
-  <a routerLink="/en" class="hover:underline">EN</a>
-  <p>/</p>
-  <a routerLink="/" class="hover:underline">ES</a>
-  </div>
-  `,
-  styleUrl: './header.component.css'
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent {
 
